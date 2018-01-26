327fbb129d5d12e7e232493352e82f1fea8f8996
syberpuppy <omri@responder.co.il>
2018-01-27 00:14:50 +0200

    Finished the animations in the new graphics

---
+++ Assets/TreeClicked.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeClicked : MonoBehaviour {
+
+    public Animator animator;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        
+
+    }
+
+    private void OnMouseUp()
+    {
+       
+        Debug.Log(" clicked");
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("tree hit");
+
+            if (hit.transform.name == "Tree")
+            {
+                Debug.Log("tree ");
+                animator.SetTrigger("Shake");
+
+            }
+        }
+    }
+
+}
