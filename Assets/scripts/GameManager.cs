5c51e0fa5c4b0deedd82ba04d633cc879bd9d14a
Udi Shenkar <udi.shenkar@gmail.com>
2018-01-26 14:31:39 +0200

    esc returns to title screen

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     public static GameManager instance = null;
@@ -29,7 +30,10 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
 	}
 
     public void NotifyClicked(Clickable c)
